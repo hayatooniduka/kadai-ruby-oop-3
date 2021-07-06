4ecd52c9b3a8ed51b672ac4495844cf092508532
hayato.oniduka <hayato.onika@gmail.com>
2021-07-06 14:50:27 +0000

    second

--- human.rb
+++ human.rb
@@ -2,11 +2,12 @@ require './animal'
 require './thinkable'
 
 class Human < Animal
+    attr_accessor :name, :age, :hoby
     include Thinkable
     
-    def initialize(name:,age:,hoby:)
-        @name=name
-        @age=age
-        @hoby=hoby
+    def initialize(name,age,hoby)
+        self.name=name
+        self.age=age
+        self.hoby=hoby
     end
 end
